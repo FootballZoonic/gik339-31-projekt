3b515812690472ff50dd7bdc8d57d97b0eb19c1a
FootballZoonic <112565074+FootballZoonic@users.noreply.github.com>
2023-12-21 15:44:54 +0100

    21/12

--- client/script.js
+++ client/script.js
@@ -1,13 +1,99 @@
 console.log("Group 31 quality");
 
-let form = document.getElementsByClassName("row");
-
+const url = "http://localhost:3000/animals";
 const btnSubmit = document.getElementById("btnSubmit");
 const btnSearch = document.getElementById("btnSearch");
 const btnUpdate = document.getElementById("btnUpdate");
 const btnDelete = document.getElementById("btnDelete");
 
-btnSubmit.addEventListener('click', (e) => console.log("submit"));
-btnSearch.addEventListener('click', (e) => console.log("search"));
-btnUpdate.addEventListener('click', (e) => console.log("update"));
-btnDelete.addEventListener('click', (e) => console.log("delete"));
+const form = document.getElementById("form");
+const table = document.getElementById("table");
+const tables = document.querySelectorAll("table");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const animal = {
+    species: form.species.value,
+    animalName: form.animalName.value,
+    weight: form.weight.value,
+    sound: form.sound.value,
+    tail: form.tail.value,
+  };
+  console.log(animal);
+
+  const request = new Request(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(animal),
+  });
+  fetch(request).then((response) => {
+    console.log(response);
+    form.reset();
+  });
+}
+
+async function getPromise() {
+  const res = await fetch("http://localhost:3000/animals");
+  return res.json();
+}
+
+//console.log(getPromise())
+
+async function getData() {
+  const animals = await getPromise();
+
+  animals.forEach((animal) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+          <td class="tbId"><input type="checkbox" aria-label="Checkbox for following text input"></td>
+          <td>${animal.id}</td>
+          <td>${animal.species}</td>
+          <td>${animal.animalName}</td>
+          <td>${animal.weight}</td>
+          <td>${animal.sound}</td>
+          <td>${animal.tail}</td>
+        `;
+    table.appendChild(row);
+  });
+}
+
+btnSubmit.addEventListener("submit", (e) => console.log("submit"));
+btnSearch.addEventListener("click", (e) => console.log("search"));
+btnUpdate.addEventListener("click", (e) => console.log("update"));
+
+btnDelete.addEventListener("click", (e) => {
+  let ids = [];
+
+  const tdElements = document.getElementsByClassName("tbId");
+
+  for (let i = 0; i < tdElements.length; i++) {
+    const checkbox = tdElements[i].querySelector('input[type="checkbox"]');
+
+    if (checkbox && checkbox.checked) {
+      const nextSibling = tdElements[i].nextElementSibling;
+      console.log(checkbox.value);
+      console.log(nextSibling.innerText);
+      ids.push(nextSibling.innerText);
+    }
+  }
+  console.log(ids);
+
+  const request = new Request(url, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(ids),
+  });
+  fetch(request).then((response) => {
+    console.log(response);
+    form.reset();
+  });
+});
+
+getData();
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
 
 server = express();
 
 server.listen(3000, () => console.log("Server running"));
 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json()); // Parse JSON bodies
 server
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
@@ -15,11 +18,28 @@ server
     next();
   });
 
-
-
 const db = new sqlite3.Database("./animals.db");
 
+// delete
 
+server.delete("/animals", (req, res) => {
+  ids = req.body;
+
+  ids.forEach(id => {
+    deleteAnimal(id);
+  });
+  res.send(ids);
+});
+// create
+server.post("/animals", (req, res) => {
+  const { species, animalName, weight, sound, tail } = req.body;
+  console.log(req.body);
+  const convertedTail = tail === "on" ? true : false;
+  createAnimal(species, animalName, weight, sound, convertedTail);
+  res.send(species);
+});
+
+// read
 server.get("/animals", (req, res) => {
   const sql = "SELECT * FROM animals";
 
@@ -32,38 +52,61 @@ server.get("/animals", (req, res) => {
   });
 });
 
-function createAnimal(species, name, weight, sound, tail)
-{
-    const insertQuery = `
-    INSERT INTO animals (species, name, weight, sound, tail)
-    VALUES ('${species}', '${name}', ${weight}, '${sound}', ${tail});
+// update
+
+server.put("/animals", (req, res) => {
+  const { id, species, animalName, weight, sound, tail } = req.body;
+  updateAnimal(id, species, animalName, weight, sound, tail);
+  res.send(id);
+});
+
+// database functions
+function updateAnimal(id, species, animalName, weight, sound, tail) {
+  const updateQuery = `
+    UPDATE animals
+    SET species = ${species}, animalName = ${animalName}, weight = ${weight}, sound = ${sound}, tail = ${tail}
+    WHERE id = ${id};
   `;
 
-        // kör query
-    db.run(insertQuery, (err) => {
-        if (err) {
-        console.log(err);
-        } else {
-        console.log(`${name} inserted!`);
-        }
-    });
+  // kör query
+  db.run(updateQuery, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`updated animal with id: ${id}`);
+    }
+  });
 }
 
+function createAnimal(species, animalName, weight, sound, tail) {
+  const insertQuery = `
+    INSERT INTO animals (species, animalName, weight, sound, tail)
+    VALUES ('${species}', '${animalName}', ${weight}, '${sound}', ${tail});
+  `;
 
+  // kör query
+  db.run(insertQuery, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`${animalName} inserted!`);
+    }
+  });
+}
 
-function createDatabase() {// skapa databas
+function createDatabase() {
+  // skapa databas
   // definiera tabell
   const createTableQuery = `
   CREATE TABLE IF NOT EXISTS animals (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     species TEXT NOT NULL,
-    name TEXT NOT NULL,
+    animalName TEXT NOT NULL,
     weight INTEGER NOT NULL,
     sound TEXT NOT NULL,
     tail BOOLEAN NOT NULL
   );
 `;
-
   // kör query
   db.run(createTableQuery, (err) => {
     if (err) {
@@ -74,22 +117,14 @@ function createDatabase() {// skapa databas
   });
 }
 
+function deleteAnimal(id) {
+  const removeAnimalQuery = `DELETE FROM animals WHERE id = ${id}`;
 
-
-function createLion() {
-  // skicka in exempel djur
-  const insertAnimalQuery = `
-  INSERT INTO animals (species, name, weight, sound, tail)
-  VALUES ('Lion', 'Leo', 40000, 'Roar', true);
-`;
-
-
-// kör query
-  db.run(insertAnimalQuery, (err) => {
+  db.run(removeAnimalQuery, (err) => {
     if (err) {
       console.log(err);
     } else {
-      console.log("lion inserted");
+      console.log(`animal with id: ${id} was removed!`);
     }
   });
 }
